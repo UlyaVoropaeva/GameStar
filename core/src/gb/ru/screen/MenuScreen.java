58f226ad9ec3882324149891b07a69caf0534800
Lenovo-PC <Ulya2811@yandex.ru>
2021-10-21 14:34:12 +0300

    lesson_3

--- core/src/gb/ru/screen/MenuScreen.java
+++ core/src/gb/ru/screen/MenuScreen.java
@@ -1,60 +1,63 @@
 package gb.ru.screen;
 
-import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import gb.ru.base.BaseScreen;
+import gb.ru.math.Rect;
+import gb.ru.sprite.Background;
+import gb.ru.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
+    private Texture bg;
     private Texture img;
-    private Texture background;
-    private Vector2 touch;
-    private Vector2 pos;
-    private Vector2 v;
-    private Vector2 tmp;
-    private static final float V_LENGHT = 2.0f;
+    private Logo logo;
+    private Background background;
 
     @Override
-    public void show(){
+    public void show() {
         super.show();
-        img = new Texture("nemo.png");
-        background = new Texture("fon.jpg");
-        touch = new Vector2();
-        pos = new Vector2();
-        v = new Vector2();
-        tmp = new Vector2();
-
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+        img = new Texture("badlogic.jpg");
+        logo = new Logo(img);
     }
 
     @Override
-    public void render (float delta){
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        logo.resize(worldBounds);
+    }
+
+    @Override
+    public void render(float delta) {
         super.render(delta);
-        tmp.set(touch);
-        if(tmp.sub(pos).len() > V_LENGHT){
-            pos.add(v);
-        } else {
-            pos.set(touch);
-        }
-        batch.begin();
-        batch.draw(background,0,0,800,480);
-        batch.draw(img, pos.x, pos.y);
-        batch.end();
-
-
+        update(delta);
+        draw();
     }
+
     @Override
-    public void dispose(){
+    public void dispose() {
         super.dispose();
+        bg.dispose();
         img.dispose();
-        background.dispose();
     }
-
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button){
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength(V_LENGHT);
-        return super.touchDown(screenX,screenY,pointer,button);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+
+        logo.touchDown(touch, pointer, button);
+        return false;
     }
 
+    private void update(float delta){
+        logo.update(delta);
+    }
+    private void draw(){
+        batch.begin();
+        background.draw(batch);
+        logo.draw(batch);
+        batch.end();
+    }
 }
